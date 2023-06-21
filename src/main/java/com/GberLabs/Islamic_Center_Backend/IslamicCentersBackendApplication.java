bd068e2363f137721ce07d91854e6ae7fad5ed65
ayoub khatouri <ayoub.khatouri@usmba.ac.ma>
2023-06-21 17:47:37 +0100

    first commit

---
+++ src/main/java/com/GberLabs/Islamic_Center_Backend/IslamicCentersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GberLabs.Islamic_Center_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IslamicCentersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IslamicCentersBackendApplication.class, args);
+	}
+
+}

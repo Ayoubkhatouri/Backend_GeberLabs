b8a4d2b8af123fdb508838c5848089f61700440b
Ammari Abdelmounaim <abdelmounaim.ammari@gmail.com>
2023-07-13 10:05:52 +0100

    updating the send email service to support styled html messages

--- src/main/java/com/GberLabs/Islamic_Center_Backend/service/EmailSenderService.java
+++ src/main/java/com/GberLabs/Islamic_Center_Backend/service/EmailSenderService.java
@@ -1,5 +1,7 @@
 package com.GberLabs.Islamic_Center_Backend.service;
 
+import jakarta.mail.MessagingException;
+
 public interface EmailSenderService {
-    int sendEmail(String toEmail ,String subject, String body);
+    int sendEmail(String toEmail ,String subject, String body) throws MessagingException;
 }

8b5548daf57e19751d10c92290fcf3c3b4516539
ayoub khatouri <ayoub.khatouri@usmba.ac.ma>
2023-08-08 18:53:36 +0100

    adding testimonial entity

---
+++ src/main/java/com/GberLabs/Islamic_Center_Backend/dtos/TestimonialDTO.java
@@ -0,0 +1,10 @@
+package com.GberLabs.Islamic_Center_Backend.dtos;
+
+import lombok.Data;
+
+@Data
+public class TestimonialDTO {
+    private Long id;
+    private String userName;
+    private String comment;
+}
---
+++ src/main/java/com/GberLabs/Islamic_Center_Backend/entities/Testimonial.java
@@ -0,0 +1,23 @@
+package com.GberLabs.Islamic_Center_Backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Testimonial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String comment;
+}
